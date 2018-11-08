6fb220604b9bc97068d9d2c30b06d0dab7689972
NozomuOhashi <ohashi@nagoya-u.jp>
2018-11-08 21:35:27 +0900

    modify run.bash

--- run/run.bash
+++ run/run.bash
@@ -22,19 +22,19 @@ start_mapping=$(rosparam get /start_mapping)
 sleep 1
 done
 
-#new terminal 4
-xterm -e bash -c 'cd ~/slam_kitti_ws/methods/Autoware/ros; source devel/setup.bash; roslaunch lidar_localizer ndt_mapping.launch' &
+#new terminal 4  
+taskset -c 0,1,2,3,4,5,6 xterm -e bash -c 'cd ~/slam_kitti_ws/methods/Autoware/ros; source devel/setup.bash; roslaunch lidar_localizer ndt_mapping.launch' &
 sleep 3
 
 if [ $current_sequence -lt 10 ] ;then
 #new terminal 2
 
-xterm -e bash -c "cd ~/slam_kitti_ws/mypkg/catkin_ws/; source devel/setup.bash; rosrun kitti_player kitti_player -d /media/ohashi/e5c3c17e-1d77-4109-ae86-cb7c80ff9c1c/share/rosbag/Kitti/odometry/dataset/sequences/0$current_sequence --velodyne 1 --grayscale 1 --color 1 --groundtruth 1 --timestamps 1 --frequency 0.5 /sensor/camera/grayscale/left/image_rect:=/sensor/camera/grayscale/left/image_raw /sensor/camera/grayscale/right/image_rect:=/sensor/camera/grayscale/right/image_raw" &
+taskset -c 7 xterm -e bash -c "cd ~/slam_kitti_ws/mypkg/catkin_ws/; source devel/setup.bash; rosrun kitti_player kitti_player -d /media/ohashi/e5c3c17e-1d77-4109-ae86-cb7c80ff9c1c/share/rosbag/Kitti/odometry/dataset/sequences/0$current_sequence --velodyne 1 --grayscale 1 --color 1 --groundtruth 1 --timestamps 1 --frequency 5 /sensor/camera/grayscale/left/image_rect:=/sensor/camera/grayscale/left/image_raw /sensor/camera/grayscale/right/image_rect:=/sensor/camera/grayscale/right/image_raw" &
 sleep 1
 
 else
 #new terminal 2
-xterm -e bash -c "cd ~/slam_kitti_ws/mypkg/catkin_ws/; source devel/setup.bash; rosrun kitti_player kitti_player -d /media/ohashi/e5c3c17e-1d77-4109-ae86-cb7c80ff9c1c/share/rosbag/Kitti/odometry/dataset/sequences/$current_sequence --velodyne 1 --grayscale 1 --color 1 --groundtruth 1 --timestamps 1 --frequency 0.5 /sensor/camera/grayscale/left/image_rect:=/sensor/camera/grayscale/left/image_raw /sensor/camera/grayscale/right/image_rect:=/sensor/camera/grayscale/right/image_raw" &
+xterm -e bash -c "cd ~/slam_kitti_ws/mypkg/catkin_ws/; source devel/setup.bash; rosrun kitti_player kitti_player -d /media/ohashi/e5c3c17e-1d77-4109-ae86-cb7c80ff9c1c/share/rosbag/Kitti/odometry/dataset/sequences/$current_sequence --velodyne 1 --grayscale 1 --color 1 --groundtruth 1 --timestamps 1 --frequency 5 /sensor/camera/grayscale/left/image_rect:=/sensor/camera/grayscale/left/image_raw /sensor/camera/grayscale/right/image_rect:=/sensor/camera/grayscale/right/image_raw" &
 sleep 1
 
 fi
